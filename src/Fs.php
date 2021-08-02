9b8c1fa08f3e372af602a7d220806dd72dce92fb
yonatonreid <yonatonreid@gmail.com>
2021-08-02 00:35:42 -0400

    several impls

--- src/Fs.php
+++ src/Fs.php
@@ -3,12 +3,18 @@
 
 namespace Cabana;
 
+use Exception;
 use function chmod;
 use function is_dir;
 use function mkdir;
 
 class Fs
 {
+    public static function fixDirectorySeparator(string $path): string
+    {
+        return Strings ::strIreplace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+    }
+
     public static function mkdir(string $path, int $perms = 0777, bool $recursive = true): void
     {
         mkdir($path, $perms, $recursive);
@@ -19,13 +25,36 @@ class Fs
         return is_dir($path);
     }
 
+    public static function mkDirIfNotExists(string $path, int $perms = 0777, bool $recursive = true): void
+    {
+        if (!static ::isDir($path)) {
+            static ::mkdir($path, $perms, $recursive);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function makeReadable(string $path, int $perms = 0777): void
+    {
+        if (!static ::isReadable($path)) {
+            static ::chmod($path, $perms);
+        }
+    }
+
     public static function isReadable(string $path): bool
     {
         return is_readable($path);
     }
 
+    /**
+     * @throws Exception
+     */
     public static function chmod(string $path, int $perms = 0777): void
     {
+        if (!static ::isDir($path)) {
+            throw new Exception("$path is not a valid path to change permissions on.");
+        }
         chmod($path, $perms);
     }
 }
