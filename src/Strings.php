24146698092c054d3fc7910745c8a92bd5a7125f
yonatonreid <yonatonreid@gmail.com>
2021-08-02 00:18:00 -0400

    strings class strireplace method name update

--- src/Strings.php
+++ src/Strings.php
@@ -8,7 +8,7 @@ use function str_ireplace;
 
 class Strings
 {
-    public static function strIReplace(array|string $search, array|string $replace, array|string $subject, $count = null): array|string
+    public static function strIreplace(array|string $search, array|string $replace, array|string $subject, $count = null): array|string
     {
         if (is_null($count)) {
             return str_ireplace($search, $replace, $subject);
