e9060d57edad88da849417de64243b751bbafe0b
yonatonreid <yonatonreid@gmail.com>
2021-08-02 00:19:48 -0400

    strings class strireplace refactoring

--- src/Strings.php
+++ src/Strings.php
@@ -8,11 +8,8 @@ use function str_ireplace;
 
 class Strings
 {
-    public static function strIreplace(array|string $search, array|string $replace, array|string $subject, int $count = null): array|string
+    public static function strIreplace(array|string $search, array|string $replace, array|string $subject, int &$count = null): array|string
     {
-        if (is_null($count)) {
-            return str_ireplace($search, $replace, $subject);
-        }
         return str_ireplace($search, $replace, $subject, $count);
     }
 }
