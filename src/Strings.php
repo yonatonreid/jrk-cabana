8ad28d72ed842c87bb21c107046555e0b61468d8
yonatonreid <yonatonreid@gmail.com>
2021-08-02 00:18:36 -0400

    strings class strireplace refactoring

--- src/Strings.php
+++ src/Strings.php
@@ -8,13 +8,10 @@ use function str_ireplace;
 
 class Strings
 {
-    public static function strIreplace(array|string $search, array|string $replace, array|string $subject, $count = null): array|string
+    public static function strIreplace(array|string $search, array|string $replace, array|string $subject, int $count = null): array|string
     {
         if (is_null($count)) {
             return str_ireplace($search, $replace, $subject);
-        }
-        if (!is_int($count)) {
-            throw new Exception("Count must be an integer.");
         }
         return str_ireplace($search, $replace, $subject, $count);
     }
