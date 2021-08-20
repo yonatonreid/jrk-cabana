b6048ffc00815f728a8a45e4fd1d7ac274a96fbd
yonatonreid <yonatonreid@gmail.com>
2021-08-19 22:26:37 -0400

    added ucwords function and array functions

--- src/Arrays.php
+++ src/Arrays.php
@@ -3,10 +3,13 @@
 
 namespace Cabana;
 
+use JetBrains\PhpStorm\Pure;
+use function array_change_key_case;
+use function array_key_exists;
 
 class Arrays
 {
-    public static function arrayKeyExists($key, $arr)
+    public static function arrayKeyExists($key, $arr): bool
     {
         return array_key_exists($key, $arr);
     }
@@ -19,5 +22,45 @@ class Arrays
             $final[$col] = $data[0];
         }
         return $final;
+    }
+
+    #[Pure] public static function arrayChangeKeyCaseUpper(array $array): array
+    {
+        return static ::arrayChangeKeyCase($array, CASE_UPPER);
+    }
+
+    #[Pure] public static function arrayChangeKeyCaseLower(array $array): array
+    {
+        return static ::arrayChangeKeyCase($array);
+    }
+
+    public static function arrayChangeKeyCaseRecursiveUpper(array $array): array
+    {
+        return static ::arrayChangeKeyCaseRecursive($array, CASE_UPPER);
+    }
+
+    public static function arrayChangeKeyCaseRecursiveLower(array $array): array
+    {
+        return static ::arrayChangeKeyCaseRecursive($array);
+    }
+
+    public static function arrayChangeKeyCaseRecursive(array $array, int $case = CASE_LOWER): array
+    {
+        return static ::arrayMap(function ($item) use ($case) {
+            if (is_array($item)) {
+                $item = static ::arrayChangeKeyCaseRecursive($item, $case);
+            }
+            return $item;
+        }, static ::arrayChangeKeyCase($array, $case));
+    }
+
+    private static function arrayChangeKeyCase(array $array, int $case = CASE_LOWER): array
+    {
+        return array_change_key_case($array, $case);
+    }
+
+    public static function arrayMap(callable $callable, array $array, array ...$arrays): array
+    {
+        return call_user_func_array('array_map', func_get_args());
     }
 }
--- src/Strings.php
+++ src/Strings.php
@@ -3,7 +3,6 @@
 
 namespace Cabana;
 
-use Exception;
 use function str_ireplace;
 
 class Strings
@@ -11,5 +10,10 @@ class Strings
     public static function strIreplace(array|string $search, array|string $replace, array|string $subject, int &$count = null): array|string
     {
         return str_ireplace($search, $replace, $subject, $count);
+    }
+
+    public static function ucWords(string $str, string $destSep = '_', string $srcSep = '_')
+    {
+        return str_replace(' ', $destSep, ucwords(str_replace($srcSep, ' ', $str)));
     }
 }
